b3127b6f3ac692096543c9b3b9412565b0a747f7
unknown <Administrator@DT63-PC.yomaridomain.com>
2016-10-20 08:34:14 +0545

    Added google sheets api

--- index.php
+++ index.php
@@ -12,15 +12,30 @@
   <!--[if lt IE 9]>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
   <![endif]-->
+
+  <script src="https://apis.google.com/js/client.js?onload=checkAuth"></script>
 </head>
 
 <body>
+   <div id="authorize-div" style="display: none">
+      <span>Authorize access to Google Sheets API</span>
+      <!--Button for the user to click to initiate auth sequence -->
+      <button id="authorize-button" onclick="handleAuthClick(event)">
+        Authorize
+      </button>
+    </div>
 
+    <pre id="output"></pre>
+    
   <div id="container" style="width:600px;height:400px;"></div>
+
+
+
+
   <script src="resources/libs/jquery-1.8.2.min.js"></script>
   <script src="resources/libs/highcharts.js"></script>
   <script src="resources/libs/drilldown.js"></script>
-  <!--<script src="resources/js/areaChart.js"></script>-->
+  <script src="resources/js/googleSheets.js"></script>
   <script src="resources/js/barDrilldown.js"></script>
 </body>
 </html>
